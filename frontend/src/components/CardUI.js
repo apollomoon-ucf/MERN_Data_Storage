1c1f400910fb6bedf14f36b78b7d1e49f26e965e
brian_moon <apollomoon@knights.ucf.edu>
2021-10-19 23:23:56 -0400

    logging in, searching and adding data

--- frontend/src/components/CardUI.js
+++ frontend/src/components/CardUI.js
@@ -1,24 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CardUI() {
+  var card = "";
+  var search = "";
+
+  const [message, setMessage] = useState("");
+  const [searchResults, setResults] = useState("");
+  const [cardList, setCardList] = useState("");
+
+  var _ud = localStorage.getItem("user_data");
+  var ud = JSON.parse(_ud);
+  var userId = ud.id;
+  var firstName = ud.firstName;
+  var lastName = ud.lastName;
+
   const addCard = async (event) => {
     event.preventDefault();
 
-    alert("addCard()");
+    var obj = { userId: userId, card: card.value };
+    var js = JSON.stringify(obj);
+
+    try {
+      const response = await fetch("http://localhost:5000/api/addcard", {
+        method: "POST",
+        body: js,
+        headers: { "Content-Type": "application/json" },
+      });
+
+      var txt = await response.text();
+      var res = JSON.parse(txt);
+
+      if (res.error.length > 0) {
+        setMessage("API Error:" + res.error);
+      } else {
+        setMessage("Card has been added");
+      }
+    } catch (e) {
+      setMessage(e.toString());
+    }
   };
 
   const searchCard = async (event) => {
     event.preventDefault();
-    alert("searchCard");
+    var obj = { userId: userId, search: search.value };
+    var js = JSON.stringify(obj);
+
+    try {
+      const response = await fetch("http://localhost:5000/api/searchcards", {
+        method: "POST",
+        body: js,
+        headers: { "Content-Type": "application/json" },
+      });
+
+      var txt = await response.text();
+      var res = JSON.parse(txt);
+      var _results = res.results;
+      var resultText = "";
+      for (var i = 0; i < _results.length; i++) {
+        resultText += _results[i];
+        if (i < _results.length - 1) {
+          resultText += ", ";
+        }
+      }
+      setResults("Card(s) have been retrieved");
+      setCardList(resultText);
+    } catch (e) {
+      alert(e.toString());
+      setResults(e.toString());
+    }
   };
 
   return (
-    <div id="accessUIDiv">
-             
+    <div id="cardUIDiv">
+        
       <br />
-             
-      <input type="text" id="searchText" placeholder="Card To Search For" />
-             
+        
+      <input
+        type="text"
+        id="searchText"
+        placeholder="Card To Search For"
+        ref={(c) => (search = c)}
+      />
+        
       <button
         type="button"
         id="searchCardButton"
@@ -26,14 +89,20 @@ function CardUI() {
         onClick={searchCard}
       >
         {" "}
-        Search Card{" "}
+        Search Card
       </button>
       <br />
-      <span id="cardSearchResult"></span>
-      <p id="cardList"></p>
+      <span id="cardSearchResult">{searchResults}</span>
+      <p id="cardList">{cardList}</p>
       <br />
       <br />
-      <input type="text" id="cardText" placeholder="Card To Add" />
+      <input
+        type="text"
+        id="cardText"
+        placeholder="Card To Add"
+        ref={(c) => (card = c)}
+      />
+        
       <button
         type="button"
         id="addCardButton"
@@ -44,7 +113,7 @@ function CardUI() {
         Add Card{" "}
       </button>
       <br />
-      <span id="cardAddResult"></span> 
+      <span id="cardAddResult">{message}</span>
     </div>
   );
 }
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
+  var loginName;
+  var loginPassword;
+
+  const [message, setMessage] = useState("");
+
   const doLogin = async (event) => {
     event.preventDefault();
-    alert("doIt()");
+
+    var obj = { login: loginName.value, password: loginPassword.value };
+    var js = JSON.stringify(obj);
+
+    try {
+      const response = await fetch("http://localhost:5000/api/login", {
+        method: "POST",
+        body: js,
+        headers: { "Content-Type": "application/json" },
+      });
+
+      var res = JSON.parse(await response.text());
+
+      if (res.id <= 0) {
+        setMessage("User/Password combination incorrect");
+      } else {
+        var user = {
+          firstName: res.firstName,
+          lastName: res.lastName,
+          id: res.id,
+        };
+        localStorage.setItem("user_data", JSON.stringify(user));
+
+        setMessage("");
+        window.location.href = "/cards";
+      }
+    } catch (e) {
+      alert(e.toString());
+      return;
+    }
   };
 
   return (
@@ -11,9 +45,18 @@ function Login() {
       <form onSubmit={doLogin}>
         <span id="inner-title">Please Log In</span>
         <br />
-        <input type="text" id="loginName" placeholder="Username" />
-        <br />
-        <input type="password" id="loginPassword" placeholder="Password" />
+        <input
+          type="text"
+          id="loginName"
+          placeholder="Username"
+          ref={(c) => (loginName = c)}
+        />
+        <input
+          type="password"
+          id="loginPassword"
+          placeholder="Password"
+          ref={(c) => (loginPassword = c)}
+        />
         <br /> 
         <input
           type="submit"
@@ -23,7 +66,7 @@ function Login() {
           onClick={doLogin}
         />
       </form>
-      <span id="loginResult"></span>
+      <span id="loginResult">{message}</span>
     </div>
   );
 }
